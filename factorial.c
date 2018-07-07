d802665beafbc8942a392a84a38532017340fc7d
Karthikeyan S <karthikkevin83@gmail.com>
2018-07-07 13:46:42 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -4,10 +4,17 @@ int main()
 int n,i,f=1;
 printf("ENTER THE NUMBER:");
 scanf("%d",&n);
+  if((n==0)||(n==1))
+  {
+    printf("1");
+  }
+  else{
+    
 for(i=n;i>=1;i--)
 {
 f=f*i;
 }
 printf("%d",f);
+  }
 return 0;
 }
